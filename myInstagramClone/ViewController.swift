2b5b4663484f6566ac5bba8cc6da69614d542dad
Sheldon1995 <sheldon.wentao@gmail.com>
2020-01-31 14:12:42 -0500

    Initial Commit

---
+++ myInstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myInstagramClone
+//
+//  Created by Sheldon on 1/31/20.
+//  Copyright © 2020 wentao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
